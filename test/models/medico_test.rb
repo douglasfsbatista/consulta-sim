15edc2aa9f86115610d251efdc09f865636e51c9
Douglas Batista <batista.douglasfs@gmail.com>
2022-09-12 21:59:27 -0300

    Testes do model de médico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,9 +1,15 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
-  test 'criando medico corretamente' do
+  test 'Criando medico corretamente' do
     medico = Medico.new nome: 'Rodrigo Andrade', cpf: '706.508.980-01', email: 'rodrigo@rodrigo.com',
                         especialidade: 'Reumatologista', crm: '123456'
     assert medico.save
+  end
+
+  test 'Criando medico com o campo cpf invalido' do
+    medico = Medico.new nome: 'Maria Eduarda', cpf: '99988877766', email: 'duda@med.al',
+                        especialidade: 'Cardiologista', crm: '553344'
+    assert_not medico.save
   end
 end
