3ef5bfe2ede521083f68fde5157ec994afaf630b
Douglas Batista <batista.douglasfs@gmail.com>
2022-09-12 21:58:56 -0300

    Testes do model de paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Criando paciente com o campo nome invalido' do
+    paciente = Paciente.new nome: '', data_nascimento: '1996-09-01',
+                            cpf: '10307715450', email: 'douglas@teste.com'
+    assert_not paciente.save
+  end
+
+  test 'Criando paciente com e-mail inválido' do
+    paciente = Paciente.new nome: 'Ana Paula Macena', data_nascimento: '1996-01-11',
+                            cpf: '111.222.333-44', email: 'ana@ana.com'
+    assert_not paciente.save
+  end
 end
